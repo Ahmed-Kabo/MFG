0eaa313888507df3ce1f6d83c8d4c013a8b226fc
Ahmed-Kabo <ahmedkabo13@hotmail.com>
2020-07-29 01:44:16 +0200

    second project

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+    $("#search-icon").click(function(){
+      $("#search-content").addClass("slid");
+    });
+    $("#exit-search").click(function(){
+        $("#search-content").removeClass("slid");
+      });
+
+
+
+
+
+  });
