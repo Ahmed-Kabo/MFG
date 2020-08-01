44056f8eafe9875d0b921b8e4e2e7749ba488751
Ahmed-Kabo <ahmedkabo13@hotmail.com>
2020-08-01 16:44:58 +0200

    my last veersion

--- js/main.js
+++ js/main.js
@@ -2,18 +2,43 @@ $(document).ready(function(){
 
 
 
+  //start icon 
+
+  
+  $(".nav-item").on("click",function(){
+      $(this).addClass('active').siblings().removeClass("active");
+  });
 
 
 
 
-
-
+  // start search icon 
     $("#search-icon").click(function(){
       $("#search-content").addClass("slid");
     });
     $("#exit-search").click(function(){
         $("#search-content").removeClass("slid");
       });
+// start button to top 
+
+$(window).scroll(function(){
+
+  const myScroll = $(window).scrollTop();
+
+  if(myScroll > 400){
+    $('.to-top').css("visibility", "visible")
+  }else{
+    $('.to-top').css("visibility", "hidden")
+  }
+
+});
+
+$(".to-top").on("click", function(){
+  $("html,body").animate({
+    scrollTop : 0
+  },400);
+});
+
 
 
 
